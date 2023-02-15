3806f9d62e9c96866f5c79c70d1f2f6eca7b7881
Fastov Ilya Olegovich <iluxailuxa@gmail.com>
2023-02-15 19:25:30 +0300

    1233

--- ThreadsApp/Program.cs
+++ ThreadsApp/Program.cs
@@ -1,9 +1,14 @@
-﻿using System.Threading;
+﻿using System.ComponentModel;
+using System.Text;
+using System.Threading;
 
 namespace ThreadsApp
 {
     internal class Program
     {
+        
+        static ParameterizedThreadStart parameterizedThreadStartStream = new ParameterizedThreadStart(SaveDataToFile);
+
         static void Main(string[] args)
         {
             //Task 1-3
@@ -50,7 +55,7 @@ namespace ThreadsApp
             }
             Console.WriteLine(message);
         }
-        //Task 4 main delegate
+        //Task 4-5 main delegate
         static void TaskDelegate(object massive)
         {
             ParameterizedThreadStart maxelemstart = new ParameterizedThreadStart(MaxElem);
@@ -58,25 +63,48 @@ namespace ThreadsApp
             ParameterizedThreadStart averageelemstart = new ParameterizedThreadStart(AverageInMassive);
             Thread thread1 = new Thread(maxelemstart);
             Thread thread2 = new Thread(minelemstart);
-            Thread thread3 = new Thread(averageelemstart);
+            Thread thread3 = new Thread(averageelemstart); 
             thread1.Start(massive);
+            thread1.Join();
             thread2.Start(massive);
+            thread2.Join();
             thread3.Start(massive);
+            thread3.Join();
         }
-        //Task 4 child delegate
+        //Task 4-5 child delegate
         static void MaxElem(object massive)
-        {            
-            Console.WriteLine(((int[])massive).Max().ToString());
-            Console.WriteLine("Это читерство!");
+        {
+            int result = ((int[])massive).Max();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStartStream);
+            thread.Start(result);
         }
         static void MinElem(object massive)
         {
-            Console.WriteLine(((int[])massive).Min().ToString());
+            int result = ((int[])massive).Min();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStartStream);
+            thread.Start(result);
         }
         static void AverageInMassive(object massive)
         {
-            Console.WriteLine(((int[])massive).Average().ToString());
+            double result = ((int[])massive).Average();
+            Console.WriteLine(result.ToString());
+            Thread thread = new Thread(parameterizedThreadStartStream);
+            thread.Start(result);
         }
+        static void SaveDataToFile(object number)
+        {
+            FileStreamOptions options = new FileStreamOptions();
+            options.Access = FileAccess.Write;
+            options.Share = FileShare.Write;
+            options.Mode = FileMode.Append;
+
+            StreamWriter writer = new StreamWriter("task5.txt", Encoding.UTF8, options);            
+            writer.WriteLine(number.ToString());
+            writer.Close();
+        }
+
     }
     //Task 1-3 class as object for task 1-3 delegate
     class MyObject 
